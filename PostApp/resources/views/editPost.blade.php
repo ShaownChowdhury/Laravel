669f4d15b5f588da8fb20f7c23b8f90e2b831deb
Shaown Chowdhury <shaownchowdhuryofficial@gmail.com>
2024-02-29 22:16:32 +0600

    edit post file

---
+++ PostApp/resources/views/editPost.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.frontendLayouts')
+
+@section('title','Add Post')
+
+@section('content')
+
+<section>
+    <div class="container">
+        <div class="card col-lg-5 mx-auto mt-5">
+            <div class="card-header">
+                Edit Post
+            </div>
+            <div class="card-body">
+ 
+            
+               
+                <form action="" method="POST">
+
+
+                    @csrf
+                    <input value="{{ $post->title }}" name="title" type="text" placeholder="Post Title" class="form-control mb-2">
+                    @error('title')
+                        <span class="text-danger">{{ $message }} </span>
+                    @enderror
+
+                    <input value="{{ $post->detail }}" name="detail" type="text" placeholder="Post Detail" class="form-control mb-2">
+                    @error('detail')
+                    <span class="text-danger">{{ $message }} </span>
+                    @enderror
+                  
+                    {{-- <textarea class="form-control mb-2" name="area" placeholder="area">{{ old('area') }}</textarea> --}}
+                    
+                    <input value="{{ $post->author }}" name="author" type="text" placeholder="Author Name" class="form-control mb-2">
+                    @error('author')
+                    <span class="text-danger">{{ $message }} </span>
+                    @enderror
+
+                    <button class="btn btn-primary">Update</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
