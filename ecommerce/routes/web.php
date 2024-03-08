2ecf2f2e1894d433ba271c52c0679e7843c09925
Shaown Chowdhury <shaownchowdhuryofficial@gmail.com>
2024-03-08 15:10:39 +0600

    added new routes

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\DashboardController;
 use App\Http\Controllers\Frontend\HomepageController;
 use Illuminate\Support\Facades\Auth;
@@ -12,3 +13,13 @@ Route::get('/',[HomepageController::class,'homepage'])->name('homepage');
 Auth::routes();
 
 Route::get('/dashboard', [DashboardController::class,'dashboard'])->name('dashboard');
+
+Route::get('/dashboard/category', [CategoryController::class,'category'])->name('category');
+
+Route::post('/dashboard/category', [CategoryController::class,'categoryInsert'])->name('category.insert');
+
+Route::get('/dashboard/categoryDelete/{id}', [CategoryController::class,'categoryDelete'])->name('category.delete');
+
+Route::get('/dashboard/categoryEdit/{id}', [CategoryController::class,'categoryEdit'])->name('category.edit');
+
+Route::put('/dashboard/categoryUpdate/{id}', [CategoryController::class,'categoryUpdate'])->name('category.update');
