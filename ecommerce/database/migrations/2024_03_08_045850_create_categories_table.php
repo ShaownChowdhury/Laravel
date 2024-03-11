3e8557b7c5cd087a569ee4b06e07ffc2b41c00e5
Shaown Chowdhury <shaownchowdhuryofficial@gmail.com>
2024-03-11 10:12:25 +0600

    migration

--- ecommerce/database/migrations/2024_03_08_045850_create_categories_table.php
+++ ecommerce/database/migrations/2024_03_08_045850_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('category');
-            $table->string('category_slug');
+            $table->string('category_slug')->unique();
             $table->timestamps();
         });
     }
