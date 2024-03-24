1d3aa78c671366006a779231004a6c938ad2db79
Shaown Chowdhury <shaownchowdhuryofficial@gmail.com>
2024-03-24 11:06:34 +0600

    laravel class 8 product

---
+++ ecommerce/app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        "title",
+        "slug",
+        "featured_img",
+        "price",
+        "selling_price",
+        "sku",
+        "brand",
+        "stock",
+        "status",
+        "featured",
+        "short_detail",
+        "long_detail",
+        "cross_sell"
+    ] ;
+    // protected $guarded = ["id"];
+}
