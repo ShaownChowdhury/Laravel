7ab81684328c85b8c4e43190475f1679d0af7cc9
Shaown Chowdhury <shaownchowdhuryofficial@gmail.com>
2024-05-25 10:18:13 +0600

    class 11

--- ecommerce/app/Http/Controllers/Frontend/ProductController.php
+++ ecommerce/app/Http/Controllers/Frontend/ProductController.php
@@ -15,10 +15,21 @@ class ProductController extends Controller
         $category = Category::select('id','category')->where('slug',$slug)->first();
         // dd($category);
 
-        $products = Product::with('galleries')->whereHas('categories',function ($q) use ($slug){
+        $products = Product::with(['galleries' => function($query){
+            return $query->select('id','product_id','title')->first();
+        }])
+        ->whereHas('categories',function ($q) use ($slug){
           return $q->where('slug',$slug);
-        })->get();
+        })->where('status',true)
+        ->select('id','title','slug','featured_img','price','selling_price','status')
+        ->get();
         // dd($products);
         return view('frontend.categoryArchieve',compact('products','category'));
+    }
+
+    function showProduct($slug){
+        $product = Product::with('galleries')->where('slug',$slug)->first();
+        // dd($product);
+     return view('frontend.single-product',compact('product'));
     }
 }
