3b89463dc8ca8fa727cda6718a1ec76a67eb5ddb
Shaown Chowdhury <shaownchowdhuryofficial@gmail.com>
2024-06-15 13:23:08 +0600

    class 13

--- ecommerce/routes/frontend.php
+++ ecommerce/routes/frontend.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Frontend\Auth\LoginController;
+use App\Http\Controllers\Frontend\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\ProductController;
 use App\Http\Controllers\Frontend\HomepageController;
@@ -9,6 +11,13 @@ Route::get('/category{slug}',[ProductController::class,'showCategoryProduct'])->
 Route::get('/product/{slug}',[ProductController::class,'showProduct'])->name('product.show');
 
 Route::get('/products/search',[ProductController::class,'searchProduct'])->name('product.search');
+
+
+Route::get('/sign-in',[LoginController::class,'showloginForm'])->name('signin');
+Route::post('/sign-in',[LoginController::class,'login'])->name('signin.verify');
+
+Route::get('/sign-up',[RegisterController::class,'showRegistrationForm'])->name('signup');
+Route::post('/sign-up',[RegisterController::class,'register'])->name('signup.store');
 
 
 ?>
