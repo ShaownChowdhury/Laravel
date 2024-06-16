e681fa966cc28ddf08f829ee01209902329c1f9e
Shaown Chowdhury <shaownchowdhuryofficial@gmail.com>
2024-06-16 19:52:24 +0600

    16 running

--- ecommerce/routes/frontend.php
+++ ecommerce/routes/frontend.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\Frontend\CartController;
 use App\Http\Controllers\Frontend\ProductController;
 use App\Http\Controllers\Frontend\HomepageController;
+use App\Http\Controllers\SslCommerzPaymentController;
 use App\Http\Controllers\Frontend\Auth\LoginController;
 use App\Http\Controllers\Frontend\Auth\SocialController;
 use App\Http\Controllers\Frontend\Auth\RegisterController;
@@ -50,6 +51,20 @@ Route::middleware('customer')->name('order.')->prefix('/order/')->controller(Ord
     Route::get('/checkout','checkout')->name('checkout');
 });
 
+
+// SSLCOMMERZ Start
+Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+
+Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
+Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+//SSLCOMMERZ END
 
 
 
