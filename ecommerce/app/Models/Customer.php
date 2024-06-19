7e06f3ca1686eea92ffd0c2afb44f473bfa17a49
Shaown Chowdhury <shaownchowdhuryofficial@gmail.com>
2024-06-19 10:51:47 +0600

    class 17

--- ecommerce/app/Models/Customer.php
+++ ecommerce/app/Models/Customer.php
@@ -12,4 +12,9 @@ class Customer extends Authenticatable
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    function hasOrder($productId) {
+        return $this->hasManyThrough(OrderItem::class, Order::class)->where('product_id', $productId)
+        ->exists();
+    }
 }
