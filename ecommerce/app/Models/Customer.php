ff58f5c16c382318498addbddf580f58d3e98c9c
Shaown Chowdhury <shaownchowdhuryofficial@gmail.com>
2024-06-19 19:37:20 +0600

    class 17

--- ecommerce/app/Models/Customer.php
+++ ecommerce/app/Models/Customer.php
@@ -14,7 +14,7 @@ class Customer extends Authenticatable
     protected $guarded = ['id'];
 
     function hasOrder($productId) {
-        return $this->hasManyThrough(OrderItem::class, Order::class)->where('product_id', $productId)
-        ->exists();
+        return $this->hasManyThrough(OrderItem::class, Order::class)->where('product_id', $productId)->exists();
     }
+
 }
