26528941756dedaccaad67d02f2d5435af0dec97
Shaown Chowdhury <shaownchowdhuryofficial@gmail.com>
2024-06-19 10:50:44 +0600

    some modification

--- ecommerce/app/Helpers/helper.php
+++ ecommerce/app/Helpers/helper.php
@@ -1,14 +1,15 @@
 <?php
 
+
 if (!function_exists("getProfileImage")) {
     function getProfileImage()
     {
-        if(auth()->user()->profile){
-            return auth()->user()->profile;
-        } else{
-            return "https:api.dicebear.com/8.x/initials/svg?seed=".auth()->user()->name;
+        if (auth()->check()) {
+            if (auth()->user()->profile) {
+                return auth()->user()->profile;
+            } else {
+                return "https://api.dicebear.com/8.x/initials/svg?seed=" . auth()->user()->name;
+            }
         }
     }
 }
-
-
