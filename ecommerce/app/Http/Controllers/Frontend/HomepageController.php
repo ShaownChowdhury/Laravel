2f50360b4fa9a1e95cc02b39b29cfb7f1cdf8776
Shaown Chowdhury <shaownchowdhuryofficial@gmail.com>
2024-06-19 22:41:34 +0600

    class 17

--- ecommerce/app/Http/Controllers/Frontend/HomepageController.php
+++ ecommerce/app/Http/Controllers/Frontend/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -11,5 +12,17 @@ class HomepageController extends Controller
     function homepage(){
         $categories = Category::latest()->take(12)->get();
         return view('frontend.homepage',compact('categories'));
+    }
+
+    function shopPage(){
+        $products = Product::with(['galleries' => function($query){
+            return $query->select('id','product_id','title')->first();
+        }])
+        
+        ->where('status',true)
+        ->select('id','title','slug','featured_img','price','selling_price','status')
+        ->paginate(15);
+        // dd($products);
+        return view('frontend.shopSidebar',compact('products'));
     }
 }
